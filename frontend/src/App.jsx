611937baea82597019a7c22932d6b4e4dcf545d3
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-07 00:27:41 -0500

    Added component folder and site background component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,3 @@
-import Ballpit from "./Ballpit"
-
 export default function App() {
-    return (
-        <div style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%'}}>
-            <Ballpit
-            count={200}
-            gravity={0.7}
-            friction={0.8}
-            wallBounce={0.95}
-            followCursor={true}
-            />
-        </div>
-    )
+
 }
