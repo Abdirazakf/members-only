09fb7bdae0e65f2659e22874acfad2666b4d1c97
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-07 21:37:30 -0500

    Added login and sign up pages as well as added routing logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,19 @@
-export default function App() {
+import {Routes, Route} from "react-router-dom"
+import {Toaster} from "react-hot-toast"
+import Navbar from "./components/Navbar"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
+export default function App() {
+    return(
+        <div className="min-h-screen bg-base-200">
+            <Navbar />
+            <Routes>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/sign-up" element={<SignUp/>} />
+            </Routes>
+
+            <Toaster />
+        </div>
+    )
 }
