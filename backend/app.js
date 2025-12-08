62a1092fa6ca8071a91955cd567eb767f05f8eca
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-08 17:29:14 -0500

    Configured backend app to serve react app from frontend

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,18 @@ app.use(express.json())
 app.use('/login', loginRouter)
 app.use('/sign-up', signupRouter)
 
+if (process.env.NODE_ENV === 'prod'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+    })
+}
+
 app.listen(process.env.PORT, (err) => {
     if (err){
         console.log(err)
     }
 
-    console.log("App is running")
+    console.log("App is running is port", process.env.PORT)
 })
