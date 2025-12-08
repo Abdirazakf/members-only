2b10aa3955d6d2fa7882b6e3ecbb15c927b8db67
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-07 23:22:20 -0500

    Configured vite to allow host url for railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/login': {
+        target: 'http://localhost:3000/login',
+        changeOrigin: true,
+        secure: false
+      }
+    },
+    allowedHosts: ['members-only-production-7933.up.railway.app']
+  },
+  preview: {
+    allowedHosts: ['members-only-production-7933.up.railway.app']
+  }
 })
