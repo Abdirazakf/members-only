dfb44112e318f083370a8a2de6d6a5063b8463ee
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-08 20:56:13 -0500

    Updated routes to use /api

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const signupRouter = require('./routes/signupRouter')
 const app = express()
 
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' 
+    origin: process.env.NODE_ENV === 'prod' 
         ? 'https://members-only-production-7933.up.railway.app'
         : 'http://localhost:5173',
     credentials: true
@@ -18,8 +18,8 @@ app.use(cors({
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.use('/login', loginRouter)
-app.use('/sign-up', signupRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/sign-up', signupRouter)
 
 if (process.env.NODE_ENV === 'prod'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
