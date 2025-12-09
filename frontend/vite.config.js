1bf244de5f58e6e48dd0d9f388d8f2cfd17dd75d
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-08 21:38:56 -0500

    Removed preview from config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,10 +11,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       }
-    },
-    allowedHosts: ['members-only-production-7933.up.railway.app']
-  },
-  preview: {
-    allowedHosts: ['members-only-production-7933.up.railway.app']
+    }
   }
 })
