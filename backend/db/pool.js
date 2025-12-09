3bb9388d1234de15c9a23a7700f1e686ecbcf1dc
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-08 21:13:06 -0500

    Updated the pool connection string depending on what node env is running

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const {Pool} = require('pg')
 
 module.exports = new Pool({
-    connectionString: process.env.DB_STRING,
+    connectionString: process.env.NODE_ENV === 'prod' ? process.env.PROD_DB : process.env.DB_STRING,
     ssl: {
         rejectUnauthorized: false,
         sslmode: 'require'
