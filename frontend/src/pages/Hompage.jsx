c7e365644a56afa3e74495e49abec1e57b8f30b3
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-09 22:44:21 -0500

    Setup logic to display different homepages for guests:

--- frontend/src/pages/Hompage.jsx
+++ frontend/src/pages/Hompage.jsx
@@ -1,5 +1,16 @@
+import { useAuthStore } from '../states/useAuthStore'
+import GuestHome from '../components/GuestHome'
+import AuthHome from '../components/AuthHome'
+import { ThreeDot } from 'react-loading-indicators'
+
 export default function Homepage(){
-    return(
-        <h1>Homepage</h1>
-    )
+    const {auth, loading} = useAuthStore()
+
+    if (loading){
+        <div className="flex items-center justify-center min-h-screen">
+            <ThreeDot color="white" size="medium" />
+        </div>
+    }
+
+    return auth ? <AuthHome /> : <GuestHome />
 }
