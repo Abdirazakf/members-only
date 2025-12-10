8d278ad3f79c6b58218a4a7069326af9f7593b4e
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-09 19:00:34 -0500

    Check if there is an authenicated user with useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+import { useAuthStore } from "./states/useAuthStore"
 import {Routes, Route} from "react-router-dom"
 import {Toaster} from "react-hot-toast"
 import Navbar from "./components/Navbar"
@@ -6,6 +8,12 @@ import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 
 export default function App() {
+    const checkAuth = useAuthStore((state) => state.checkAuth)
+
+    useEffect(() => {
+        checkAuth()
+    }, [checkAuth])
+
     return(
         <div className="min-h-screen bg-slate-950 scheme-dark">
             <Navbar />
