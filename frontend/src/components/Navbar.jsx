64396bc0d0b5258f96f24db62d5aea5bb53bfecb
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-09 19:01:19 -0500

    If there is an user logged in, thelogin and sign up buttons get replaced with a welcome message and logout button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import { Link } from "react-router-dom"
-import {LockKeyhole} from 'lucide-react'
+import { Link, useNavigate } from "react-router-dom"
+import {LockKeyhole, LogOut, User} from 'lucide-react'
+import { useAuthStore } from "../states/useAuthStore"
+import toast from 'react-hot-toast'
 
 export default function Navbar() {
+    const navigate = useNavigate()
+    const { user, auth, logoutUser} = useAuthStore()
+
+    const handleLogout = async () => {
+        await logoutUser()
+        toast.success('Logged out Successfully')
+        navigate('/')
+    }
     return (
-        <div className="mx-auto p-4 scheme-dark whitespace-nowrap border-b border-solid border-white/10 px-4 sm:px-10 py-4">
+        <div className="mx-auto p-4 scheme-dark whitespace-nowrap border-b border-solid 
+        border-white/10 px-4 sm:px-10 py-4">
             <div className="flex items-center justify-between gap-4">
                 <div className="flex items-center justify-start gap-2">
                     <LockKeyhole className="size-7 text-primary flex-shrink-0" color="#607afb"/>
@@ -14,12 +25,40 @@ export default function Navbar() {
                     </Link>
                 </div>
                 <nav className="flex items-center gap-4">
-                    <Link to={'/login'} className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-white hover:bg-white/10 h-10 px-4 py-2 rounded-lg text-sm font-medium">
-                        Login
-                    </Link>
-                    <Link to={'/sign-up'} className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-white hover:bg-white/10 h-10 px-4 py-2 rounded-lg text-sm font-medium">
-                        Sign Up
-                    </Link>
+                    {auth ? (
+                        <>
+                            <div className="flex items-center gap-2 text-white">
+                                <User className="w-5 h-5" />
+                                <span className="text-sm font-medium">
+                                    Welcome, {user.first_name}
+                                </span>
+                            </div>
+                            <button onClick={handleLogout}
+                            className="inline-flex items-center justify-center whitespace-nowrap 
+                            ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 
+                            focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 
+                            text-white hover:bg-white/10 h-10 px-4 py-2 rounded-lg text-sm font-medium">
+                                <LogOut className="w-5 h-5" />
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <Link to={'/login'} className="inline-flex items-center justify-center whitespace-nowrap 
+                            ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 
+                            focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 
+                            text-white hover:bg-white/10 h-10 px-4 py-2 rounded-lg text-sm font-medium">
+                                Login
+                            </Link>
+                            <Link to={'/sign-up'} className="inline-flex items-center justify-center whitespace-nowrap 
+                            ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 
+                            focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 
+                            text-white hover:bg-white/10 h-10 px-4 py-2 rounded-lg text-sm font-medium">
+                                Sign Up
+                            </Link>
+                        </>
+                        )
+                    }
                 </nav>
             </div>
         </div>
