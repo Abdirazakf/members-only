c3a1d1807ca4909ef5baccd408c6b55e8d60aefd
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-10 01:45:50 -0500

    Create footer component and replaced it across the site

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="mt-auto border-t border-slate-800 px-4 py-8">
+            <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-center gap-4">
+                <a href="https://www.github.com/Abdirazakf" target='_blank' className="text-sm text-slate-400">
+                    Created By Abdirazak Farah
+                </a>
+            </div>
+        </footer>
+    )
+}
--- frontend/src/pages/Info.jsx
+++ frontend/src/pages/Info.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Eye, Theater, Users } from 'lucide-react'
 import { useState } from 'react'
+import Footer from '../components/Footer'
 
 export default function LearnMore() {
     return (
@@ -168,17 +169,5 @@ function End() {
                 </button>
             </Link>
         </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className="mt-auto border-t border-slate-800 px-4 py-8">
-            <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-center gap-4">
-                <a href="https://www.github.com/Abdirazakf" target='_blank' className="text-sm text-slate-400">
-                    Created By Abdirazak Farah
-                </a>
-            </div>
-        </footer>
     )
 }
