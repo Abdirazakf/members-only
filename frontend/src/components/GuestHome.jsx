01e0294f26fdb29167f1617f75b8d61b8e992837
Abdirazakf <farahabdirazak13@gmail.com>
2025-12-10 21:43:18 -0500

    Fixed padding on page header hero

--- frontend/src/components/GuestHome.jsx
+++ frontend/src/components/GuestHome.jsx
@@ -37,7 +37,7 @@ export default function GuestHome(){
 
     return(
         <>
-            <div className="flex flex-col gap-6 px-4 py-16 text-center @[480px]:gap-8">
+            <div className="flex flex-col gap-6 px-4 pt-16 text-center @[480px]:gap-8">
                 <div className="flex flex-col gap-2">
                     <h1 className="text-white text-4xl font-bold leading-tight tracking-tight">
                         Speak Freely, Connect Anonymously.
